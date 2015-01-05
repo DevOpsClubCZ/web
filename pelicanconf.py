f81f5370ff81f46a5433fe56b2941ef1c5b6fe27
Veros Kaplan <veros@inuits.eu>
2015-01-05 23:38:55 +0100

    I'm commited

---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'DevOps Club'
+SITENAME = u'DevOps @ cz'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'cz'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+USE_CUSTOM_MENU = True
+CUSTOM_MENUITEMS = [
+    ['Brno', '{tag}brno'],
+]
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+THEME = 'themes/cait'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
